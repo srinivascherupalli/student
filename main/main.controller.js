1c450bb9df59bb73accc8d11a60bb255bc81269e
srinivascherupalli <srinivascherupalli@gmail.com>
2017-09-22 21:56:23 +1000

    a

---
+++ main/main.controller.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('MainController', MainController);
+
+    MainController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
+    function MainController($location, AuthenticationService, FlashService) {
+        var vm = this;
+
+        vm.login = login;
+
+        (function initController() {
+            // reset login status
+            
+        })();
+	}
+
+})();
